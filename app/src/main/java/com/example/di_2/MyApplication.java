03b2868a44ef0ed39d5551364703226976815a66
VanKhuong96 <vankhuongitjava@gmail.com>
2021-11-24 17:21:36 +0700

    first commit

---
+++ app/src/main/java/com/example/di_2/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.di_2;
+
+import androidx.multidex.MultiDexApplication;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApplication extends MultiDexApplication {
+
+}
